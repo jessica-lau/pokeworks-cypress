0ef4ac62e6f16587d9c995d14fe4cc3e074207aa
jessica-lau <jessica.lau2@gmail.com>
2022-02-08 19:55:36 -0500

    add elements to POM

--- cypresse2e/cypress/pages/elements/footer.js
+++ cypresse2e/cypress/pages/elements/footer.js
@@ -1,5 +1,9 @@
 class Footer {
   logo = ".footer-logo";
+  footerHeading = "h4";
+  footerDescription = "p";
+  emailInput = 'input[type="email"]';
+  sumbitEmail = ".newsletter-arrow-right";
 }
 
 export default Footer;
