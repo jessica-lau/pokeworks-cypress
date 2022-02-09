325e0f1e1bd052ec6f5e0450efb9d7d1a9f2dc69
jessica-lau <jessica.lau2@gmail.com>
2022-02-08 20:06:13 -0500

    add get methods in footer POM

--- cypresse2e/cypress/pages/elements/footer.js
+++ cypresse2e/cypress/pages/elements/footer.js
@@ -3,7 +3,77 @@ class Footer {
   footerHeading = "h4";
   footerDescription = "p";
   emailInput = 'input[type="email"]';
-  sumbitEmail = ".newsletter-arrow-right";
+  submitEmail = ".newsletter-arrow-right";
+  footerItemListOne = "#footer-column-1-nav-list > li";
+  footerItemListTwo = "#footer-column-2-nav-list > li";
+  termAndPolicy = "#footer-fineprint-nav-list > li";
+  socialMedia = "#social-icons-list > li";
+
+  constructor() {}
+
+  visit() {
+    cy.visit("");
+  }
+
+  getLogo() {
+    return cy.get(this.logo);
+  }
+
+  getFooterHeading() {
+    return cy.get(this.footerHeading);
+  }
+
+  getFooterDescription() {
+    return cy.get(this.footerDescription);
+  }
+
+  getEmailInput() {
+    return cy.get(this.emailInput);
+  }
+
+  getSubmitEmail() {
+    return cy.get(this.submitEmail);
+  }
+
+  getNutrition() {
+    return cy.get(this.footerItemListOne).eq(0);
+  }
+
+  getLocation() {
+    return cy.get(this.footerItemListOne).eq(1);
+  }
+
+  getFranchise() {
+    return cy.get(this.footerItemListOne).eq(2);
+  }
+
+  getJobs() {
+    return cy.get(this.footerItemListOne).eq(3);
+  }
+
+  getRewards() {
+    return cy.get(this.footerItemListOne).eq(4);
+  }
+
+  getStory() {
+    return cy.get(this.footerItemListTwo).eq(0);
+  }
+
+  getCatering() {
+    return cy.get(this.footerItemListTwo).eq(1);
+  }
+
+  getPress() {
+    return cy.get(this.footerItemListTwo).eq(2);
+  }
+
+  getFaqs() {
+    return cy.get(this.footerItemListTwo).eq(3);
+  }
+
+  getContact() {
+    return cy.get(this.footerItemListTwo).eq(4);
+  }
 }
 
 export default Footer;
