9b667a45af4feb77654ff376c24b1e31ec1ef551
jessica-lau <jessica.lau2@gmail.com>
2022-02-09 20:40:12 -0500

    add footer test scripts

--- cypresse2e/cypress/integration/e2etests/footer.spec.js
+++ cypresse2e/cypress/integration/e2etests/footer.spec.js
@@ -7,5 +7,16 @@ describe("Pokeworks footer section", () => {
     beforeEach(() => {
       footer.visit();
     });
+
+    it("Pokeworks logo in footer is visible", () => {
+      footer.getLogo().should("be.visible");
+    });
+
+    it("Pokeworks description is visible", () => {
+      footer.getfooterHeading().should("contain", "GET FRESH UPDATES");
+      footer
+        .getFooterDescription()
+        .should("contain", "Sign up to receive exclusive deals");
+    });
   });
 });
