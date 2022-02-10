bea3bc85947375d6e046f2e5eee0ff02e70efcfa
jessica-lau <jessica.lau2@gmail.com>
2022-02-09 20:19:49 -0500

    import Footer POM to footer spec and add before hook

---
+++ cypresse2e/cypress/integration/e2etests/footer.spec.js
@@ -0,0 +1,11 @@
+import Footer from "../../pages/elements/footer";
+
+describe("Pokeworks footer section", () => {
+  const footer = new Footer();
+
+  context("User views content and links in footer section", () => {
+    beforeEach(() => {
+      footer.visit();
+    });
+  });
+});
