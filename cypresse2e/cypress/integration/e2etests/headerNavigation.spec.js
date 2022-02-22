34ff2256c2e985d1fe155a89e1802947cb5c7e7e
jessica-lau <jessica.lau2@gmail.com>
2022-02-21 20:41:15 -0500

    change descriptions in test scripts

--- cypresse2e/cypress/integration/e2etests/headerNavigation.spec.js
+++ cypresse2e/cypress/integration/e2etests/headerNavigation.spec.js
@@ -12,7 +12,7 @@ describe("Pokeworks header section", () => {
       header.getLogo().should("be.visible").click();
     });
 
-    it("Sign In button is clickable and redirects to Sign In page", () => {
+    it("Sign In button is clickable and redirects to sign in page", () => {
       header.getSignInButton().should("be.visible").click();
       cy.url().should("include", "order");
     });
@@ -22,27 +22,27 @@ describe("Pokeworks header section", () => {
       cy.url().should("include", "order");
     });
 
-    it("Menu link in header is visible and clickable", () => {
+    it("Menu link in header is clickable and redirects to order page", () => {
       header.getMenu().should("be.visible").click();
       cy.url().should("include", "order");
     });
 
-    it("Locations link in header is visible and clickable", () => {
+    it("Locations link in header is clickable and redirects to locations page", () => {
       header.getLocations().should("be.visible").click();
       cy.url().should("include", "/locations");
     });
 
-    it("Nutrition link in header is visible and clickable", () => {
+    it("Nutrition link in header is clickable and redirects to nutrition page", () => {
       header.getNutrition().should("be.visible").click();
       cy.url().should("include", "/nutrition");
     });
 
-    it("Catering link in header is visible and clickable", () => {
+    it("Catering link in header is clickable and redirects to catering page", () => {
       header.getCatering().should("be.visible").click();
       cy.url().should("include", "/catering");
     });
 
-    it("Own a Franchise link in header is visible and clickable", () => {
+    it("Own a Franchise link in header is clickable and redirects to franchise page", () => {
       header.getFranchise().should("be.visible").click();
       cy.url().should("include", "/franchise-with-pokeworks");
     });
