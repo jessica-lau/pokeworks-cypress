93c58b5445e434d4862ee321edbf499c94a79796
jessica-lau <jessica.lau2@gmail.com>
2022-03-15 19:40:18 -0400

    add elements for Header POM

---
+++ cypresse2e/cypress/pages/elements/headerNavigation.js
@@ -0,0 +1,9 @@
+class Header {
+  logo = ".logo-desktop-wrapper";
+  navigationMenu = "#header-nav-list li";
+  accountAndOrder = ".account-n-order-wrapper a";
+
+  constructor() {}
+}
+
+export default Header;
