b410830d928c462d4e1ef4d7757e44b0733be0a2
jessica-lau <jessica.lau2@gmail.com>
2022-03-15 20:15:51 -0400

    add elements in homepage POM

--- cypresse2e/cypress/pages/elements/homepage.js
+++ cypresse2e/cypress/pages/elements/homepage.js
@@ -1,6 +1,14 @@
 class HomePage {
   slideBanner = ".frontpage-hero";
   pokeworksRewards = ".rewards-breaker";
+  createAccount = ".rewards-cta-btn";
+  signIn = ".rewards-signin-btn";
+  sectionHeader = ".section-header";
+  foodGrid = ".pokeyourway-grid a";
+  orderAppSection = ".app-order";
+  appStore = ".app-badge-row a";
+  mobilePrevBtn = ".swiper-button-prev";
+  mobileNextBtn = ".swiper-button-next";
 
   constructor() {}
 
