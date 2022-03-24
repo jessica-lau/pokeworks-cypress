e0ec095aac49db627da0bc0c27de47628d1cbfc2
jessica-lau <jessica.lau2@gmail.com>
2022-03-24 17:42:43 -0400

    add get methods to homepage POM

--- cypresse2e/cypress/pages/elements/homepage.js
+++ cypresse2e/cypress/pages/elements/homepage.js
@@ -15,5 +15,45 @@ class HomePage {
   visit() {
     cy.visit();
   }
+
+  getSlideBanner() {
+    return cy.get(this.slideBanner);
+  }
+
+  getRewards() {
+    return cy.get(this.pokeworksRewards);
+  }
+
+  getCreateAccount() {
+    return cy.get(this.createAccount);
+  }
+
+  getSignIn() {
+    return cy.get(this.signIn);
+  }
+
+  getSectionHeader() {
+    return cy.get(this.sectionHeader);
+  }
+
+  getFoodGrid() {
+    return cy.get(this.foodGrid);
+  }
+
+  getOrderApp() {
+    return cy.get(this.orderAppSection);
+  }
+
+  getAppStore() {
+    return cy.get(this.appStore);
+  }
+
+  getPrevBtn() {
+    return cy.get(this.mobilePrevBtn);
+  }
+
+  getNextBtn() {
+    return cy.get(this.mobileNextBtn);
+  }
 }
 export default HomePage;
