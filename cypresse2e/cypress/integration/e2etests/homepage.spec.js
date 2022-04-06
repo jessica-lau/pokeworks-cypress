11a31d705480da72b5b1890d142fc35a1afd8ade
jessica-lau <jessica.lau2@gmail.com>
2022-04-05 20:12:50 -0400

    add test scripts for create account and sign in buttons

--- cypresse2e/cypress/integration/e2etests/homepage.spec.js
+++ cypresse2e/cypress/integration/e2etests/homepage.spec.js
@@ -14,5 +14,19 @@ describe("Pokeworks landing page", () => {
       homepage.getPrevBtn().eq(0).dblclick();
       homepage.getNextBtn().eq(0).dblclick();
     });
+
+    it("Pokeworks join rewards image is visible", () => {
+      homepage.getRewards().should("be.visible");
+    });
+
+    it("Create an Account button is clickable and redirects to create account page", () => {
+      homepage.getCreateAccount().contains("CREATE AN ACCOUNT").click();
+      cy.url().should("include", "order");
+    });
+
+    it("Sign In link is clickable and redirects to sign in page", () => {
+      homepage.getSignIn().contains("SIGN IN").click();
+      cy.url().should("include", "order");
+    });
   });
 });
