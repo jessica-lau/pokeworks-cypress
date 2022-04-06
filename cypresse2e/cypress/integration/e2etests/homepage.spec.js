9c89f0a0a941b6fce913212ae635922e3d20b1b8
jessica-lau <jessica.lau2@gmail.com>
2022-04-05 20:13:46 -0400

    remove emailAndPassword fixture

--- cypresse2e/cypress/integration/e2etests/homepage.spec.js
+++ cypresse2e/cypress/integration/e2etests/homepage.spec.js
@@ -1,5 +1,4 @@
 import HomePage from "../../pages/elements/homepage";
-import emailAndPassword from "../../fixtures/emailAndPassword.json";
 
 describe("Pokeworks landing page", () => {
   const homepage = new HomePage();
