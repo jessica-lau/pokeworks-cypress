a5672d458ca8ae63ff0937799785f944e87ec197
jessica-lau <jessica.lau2@gmail.com>
2022-04-09 21:57:53 -0400

    add test script for first item on food grid

--- cypresse2e/cypress/integration/e2etests/homepage.spec.js
+++ cypresse2e/cypress/integration/e2etests/homepage.spec.js
@@ -36,6 +36,9 @@ describe("Pokeworks landing page", () => {
         .and("contain", "Keep it 100");
     });
 
-    it("Food options are visible, clickable, and redirects to order page", () => {});
+    it("Food options are visible, clickable, and redirects to order page", () => {
+      homepage.getFoodGrid().eq(0).should("be.visible").click();
+      cy.url().should("include", "order");
+    });
   });
 });
