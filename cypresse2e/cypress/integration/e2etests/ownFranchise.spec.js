6d8684f66ba7174bf44d1f809f1b76d822f7931c
jessica-lau <jessica.lau2@gmail.com>
2022-05-10 18:50:58 -0400

    add pokeworks franchise information test script

--- cypresse2e/cypress/integration/e2etests/ownFranchise.spec.js
+++ cypresse2e/cypress/integration/e2etests/ownFranchise.spec.js
@@ -13,4 +13,14 @@ describe("Pokeworks Own a Franchise Page", () => {
   it("Slide images on Franchise page are visible", () => {
     franchise.getSlideImages().should("be.visible");
   });
+
+  it("Pokeworks franchise background information is visible", () => {
+    franchise
+      .getFranchiseInfo()
+      .contains(
+        "Pokeworks is the nation’s largest and fastest growing poke franchise",
+        "We continue to build on our segment leading poke franchise",
+        "corporate support"
+      );
+  });
 });
