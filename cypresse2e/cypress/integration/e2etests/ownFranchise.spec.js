a0d270d44ace4f0ea96d3ac823862e157452cfaf
jessica-lau <jessica.lau2@gmail.com>
2022-06-13 17:12:09 -0400

    fix syntax error

--- cypresse2e/cypress/integration/e2etests/ownFranchise.spec.js
+++ cypresse2e/cypress/integration/e2etests/ownFranchise.spec.js
@@ -5,16 +5,16 @@ describe("Pokeworks Own a Franchise Page", () => {
   const franchise = new Franchise();
 
   context("User views and applies for franchise on Franchise page", () => {
+
     beforeEach(() => {
       franchise.visit();
     });
-  });
 
-  it("Slide images on Franchise page are visible", () => {
+    it("Slide images on Franchise page are visible", () => {
     franchise.getSlideImages().should("be.visible");
-  });
+    });
 
-  it("Pokeworks franchise background information is visible", () => {
+    it("Pokeworks franchise background information is visible", () => {
     franchise
       .getFranchiseInfo()
       .contains(
@@ -22,13 +22,13 @@ describe("Pokeworks Own a Franchise Page", () => {
         "We continue to build on our segment leading poke franchise",
         "corporate support"
       );
-  });
+    });
 
-  it("Form heading should be visible", () => {
+    it("Form heading should be visible", () => {
     franchise.getFranchiseFormHeading().contains("multi-unit franchise owners");
-  });
+    });
 
-  it("User fills out form with details", () => {
+    it("User fills out form with details", () => {
     franchise.getFranchiseFormDetails().eq(0).click().type("Jane");
     franchise.getFranchiseFormDetails().eq(1).click().type("Doe");
     franchise.getFranchiseFormDetails().eq(2).click().type("333-333-3333");
@@ -47,34 +47,35 @@ describe("Pokeworks Own a Franchise Page", () => {
       .getFranchiseExperience()
       .click()
       .type("Opened local pizza shop in lower Manhattan for 5 years.");
-  });
+    });
 
-  it("Form Submit button is visible and clickable", () => {
+    it("Form Submit button is visible and clickable", () => {
     cy.get("input").should("have.value", "Submit").click()
-  })
+    });
 
-  it("Video on Franchise page is visible and playable", () => {
+    it("Video on Franchise page is visible and playable", () => {
     franchise.getYoutubeVideo().should("be.visible").dblclick()
-  })
+    });
 
-  it("Images on Franchise page are visible", () => {
+    it("Images on Franchise page are visible", () => {
     franchise.getPokeWorksImages().eq(0).should("be.visible")
     franchise.getPokeWorksImages().eq(1).should("be.visible")
     franchise.getPokeWorksImages().eq(2).should("be.visible")
-  })
+    });
 
-  it("Social media icons on Franchise page is visible and clickable", () => {
+    it("Social media icons on Franchise page is visible and clickable", () => {
     franchise.getSocialMedia().eq(0).should("be.visible").click();
     franchise.getSocialMedia().eq(1).should("be.visible").click();
     franchise.getSocialMedia().eq(2).should("be.visible").click();
-  });
+    });
 
-  it("Privacy Policy link on the Franchise page is visible and clickable", () => {
+    it("Privacy Policy link on the Franchise page is visible and clickable", () => {
     franchise.getPrivacyPolicy().should("be.visible").click();
     cy.url().includes("privacy-policy");
-  });
+   });
 
   //negative scenarios
+
   it("Leaving all fields on form blank and clicking Submit triggers error", () => {
     cy.get("input").should("have.value", "Submit").click()
     franchise.getFormError().eq(0).should("contain", "The field is required.")
@@ -85,7 +86,7 @@ describe("Pokeworks Own a Franchise Page", () => {
     franchise.getFormError().eq(7).should("contain", "The field is required.")
     franchise.getFormError().eq(10).should("contain", "The field is required.")
     franchise.getTryAgainError().should("contain", "One or more fields have an error. Please check and try again.")
-  })
+  });
 
   it("Populating email field with invalid email triggers error", () => {
     franchise.getFranchiseFormDetails().eq(0).click().type("Jane");
@@ -109,5 +110,6 @@ describe("Pokeworks Own a Franchise Page", () => {
       cy.get("input").should("have.value", "Submit").click();
       franchise.getFormError().eq(3).should("contain", "The e-mail address entered is invalid.");
       franchise.getTryAgainError().should("contain", "One or more fields have an error. Please check and try again.")
-  })
+    });
+  });
 });
