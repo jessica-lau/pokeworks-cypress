e07e410f2c4575e712fe4c267611f0817fe8ef46
jessica-lau <jessica.lau2@gmail.com>
2022-09-09 17:21:55 -0400

    add constant and beforeEach hook

--- cypresse2e/cypress/integration/e2etests/whyPokeworks.spec.js
+++ cypresse2e/cypress/integration/e2etests/whyPokeworks.spec.js
@@ -1,3 +1,11 @@
 import WhyPokeworks from "../../pages/elements/whyPokeworks";
 
-describe("Why Pokeworks page", () => {});
+describe("Why Pokeworks page", () => {
+  const whyPokeworks = new WhyPokeworks();
+
+  context("User views content on Why Pokeworks page", () => {
+    beforeEach(() => {
+      whyPokeworks.visit();
+    });
+  });
+});
