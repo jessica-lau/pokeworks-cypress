48aa0b3b828eaa3ba8e87a46b578fcb7b726d79b
jessica-lau <jessica.lau2@gmail.com>
2022-09-12 22:27:49 -0400

    add elements in POM for sections in why pokeworks page

--- cypresse2e/cypress/pages/elements/whyPokeworks.js
+++ cypresse2e/cypress/pages/elements/whyPokeworks.js
@@ -1,4 +1,9 @@
 class WhyPokeworks {
+  subNavigation = ".secondary_nav";
+  pokeworksHeading = "h1";
+  sectionHeading = "h2";
+  sectionDescription = ".section-description";
+  sectionButton = ".btn-outlined btn-primary";
   constructor() {}
 
   visit() {
