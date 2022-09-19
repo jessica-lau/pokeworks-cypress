d189c04b435cfb8ea5c1730963be140b3c5b4830
jessica-lau <jessica.lau2@gmail.com>
2022-09-19 12:00:55 -0400

    add test scripts for subnavigation links

--- cypresse2e/cypress/integration/e2etests/whyPokeworks.spec.js
+++ cypresse2e/cypress/integration/e2etests/whyPokeworks.spec.js
@@ -8,4 +8,28 @@ describe("Why Pokeworks page", () => {
       whyPokeworks.visit();
     });
   });
+
+  it("First subnavigation is visible and clickable", () => {
+    whyPokeworks.getsubNavigation().eq(0).should("be.visible").click();
+  });
+
+  it("Second subnavigation is visible and clickable", () => {
+    whyPokeworks.getsubNavigation().eq(1).should("be.visible").click();
+  });
+
+  it("Third subnavigation is visible and clickable", () => {
+    whyPokeworks.getsubNavigation().eq(2).should("be.visible").click();
+  });
+
+  it("Fourth subnavigation is visible and clickable", () => {
+    whyPokeworks.getsubNavigation().eq(3).should("be.visible").click();
+  });
+
+  it("Fifth subnavigation is visible and clickable", () => {
+    whyPokeworks.getsubNavigation().eq(4).should("be.visible").click();
+  });
+
+  it("Sixth subnavigation is visible and clickable", () => {
+    whyPokeworks.getsubNavigation().eq(5).should("be.visible").click();
+  });
 });
