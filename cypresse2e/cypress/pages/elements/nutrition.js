ad40c4566eafad60a442aa331f07635c8ff9988b
jessica-lau <jessica.lau2@gmail.com>
2022-10-28 18:03:32 -0400

    add additional elements to nutrition POM

--- cypresse2e/cypress/pages/elements/nutrition.js
+++ cypresse2e/cypress/pages/elements/nutrition.js
@@ -2,6 +2,9 @@ class Nutrition {
   nutritionHeading = ".foreground h1";
   nutritionDescription = ".page-content-wrapper > p > span";
   menuHeading = "h2";
+  searchBarMenu = ".form-control";
+  filterDropdown = ".multiselect__tags";
+  menuList = "ul > li";
 
   constructor() {}
 
