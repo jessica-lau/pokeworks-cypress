4ea3a6145c458a2fbde1cb21534f33c2d3c03516
jessica-lau <jessica.lau2@gmail.com>
2022-10-31 11:37:27 -0400

    add before hook and create nutrition constant

--- cypresse2e/cypress/integration/e2etests/nutrition.spec.js
+++ cypresse2e/cypress/integration/e2etests/nutrition.spec.js
@@ -1,1 +1,11 @@
 import Nutrition from "../../pages/elements/nutrition";
+
+describe("Pokeworks Nutrition page", () => {
+  const nutrition = new Nutrition();
+
+  context("User views Nutrition page and interacts with nutrition menu", () => {
+    beforeEach(() => {
+      nutrition.visit();
+    });
+  });
+});
