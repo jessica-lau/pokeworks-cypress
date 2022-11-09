26c74f95ec77933394c81e374fa6bf43093b3d9c
jessica-lau <jessica.lau2@gmail.com>
2022-11-08 20:13:53 -0500

    add test script for choosing an allergen option for in the filter

--- cypresse2e/cypress/integration/e2etests/nutrition.spec.js
+++ cypresse2e/cypress/integration/e2etests/nutrition.spec.js
@@ -38,6 +38,7 @@ describe("Pokeworks Nutrition page", () => {
     it("User can choose and add an allergen filter from the dropdown", () => {
       nutrition.getFilterToggle().click();
       nutrition.getFilterDropdown().click();
+      nutrition.getFilterDropdownChoice().eq(3).click();
     });
   });
 });
