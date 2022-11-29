50f458c23925081a05ef5cd95316db6c74947ba9
jessica-lau <jessica.lau2@gmail.com>
2022-11-29 15:53:50 -0500

    add beforeEach hook for visiting catering page

--- cypresse2e/cypress/integration/e2etests/catering.spec.js
+++ cypresse2e/cypress/integration/e2etests/catering.spec.js
@@ -2,4 +2,10 @@ import Catering from "../../pages/elements/catering";
 
 describe("Pokeworks Catering page", () => {
   const catering = new Catering();
+
+  context("User views content on Catering page", () => {
+    beforeEach(() => {
+      catering.visit();
+    });
+  });
 });
