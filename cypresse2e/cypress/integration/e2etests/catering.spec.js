a529e65a99e7812e32aae739d75559296b1aa70e
jessica-lau <jessica.lau2@gmail.com>
2022-12-22 13:42:25 -0500

    add click command for buttons

--- cypresse2e/cypress/integration/e2etests/catering.spec.js
+++ cypresse2e/cypress/integration/e2etests/catering.spec.js
@@ -66,9 +66,9 @@ describe("Pokeworks Catering page", () => {
     });
 
     it("User can select all size options", () => {
-      catering.getSizeOptionBtn.eq(0).should("contain", "10 - 25");
-      catering.getSizeOptionBtn.eq(1).should("contain", "26 - 75");
-      catering.getSizeOptionBtn.eq(2).should("contain", "76+");
+      catering.getSizeOptionBtn.eq(0).should("contain", "10 - 25").click();
+      catering.getSizeOptionBtn.eq(1).should("contain", "26 - 75").click();
+      catering.getSizeOptionBtn.eq(2).should("contain", "76+").click();
     });
   });
 });
