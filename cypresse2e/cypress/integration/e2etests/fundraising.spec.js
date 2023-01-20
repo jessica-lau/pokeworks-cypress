b8cfc182ea9b4c5225708fd9f4d0461213a20a0c
jessica-lau <jessica.lau2@gmail.com>
2023-01-20 17:47:46 -0500

    add beforeEach hook

--- cypresse2e/cypress/integration/e2etests/fundraising.spec.js
+++ cypresse2e/cypress/integration/e2etests/fundraising.spec.js
@@ -2,4 +2,10 @@ import Fundraising from "../../pages/elements/fundraising";
 
 describe("Pokeworks Fundraising page", () => {
   const fundraising = new Fundraising();
+
+  context("User views Fundraising page", () => {
+    beforeEach(() => {
+      fundraising.visit();
+    });
+  });
 });
