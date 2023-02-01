02bca6d49b141a77939dfb18721d52372220ac0c
jessica-lau <jessica.lau2@gmail.com>
2023-02-01 16:32:52 -0500

    add elements in locations POM

--- cypresse2e/cypress/pages/elements/locations.js
+++ cypresse2e/cypress/pages/elements/locations.js
@@ -1,4 +1,10 @@
 class Locations {
+  locationsHeading = "#locations-heading";
+  searchBar = "#sl-main-search";
+  searchBtn = ".sl-search-btn";
+  statesTab = "#sl-category-tab";
+  milesTab = "#sl-distance-tab";
+
   constructor() {}
 
   visit() {
