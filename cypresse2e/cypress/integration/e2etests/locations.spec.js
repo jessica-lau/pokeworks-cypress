cdab1c764f8e2129291a3648b01b8bc9af71ff5c
jessica-lau <jessica.lau2@gmail.com>
2023-02-06 14:13:43 -0500

    add beforeEach hook in spec

--- cypresse2e/cypress/integration/e2etests/locations.spec.js
+++ cypresse2e/cypress/integration/e2etests/locations.spec.js
@@ -1,1 +1,11 @@
 import Locations from "../../pages/elements/locations";
+
+describe("Pokeworks Locations page", () => {
+  const locations = new Locations();
+
+  context("User views content Locations page", () => {
+    beforeEach(() => {
+      locations.visit();
+    });
+  });
+});
