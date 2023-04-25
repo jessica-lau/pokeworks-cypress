c45ee6397367b43476a67474a3ef68b4db986c88
jessica-lau <jessica.lau2@gmail.com>
2023-04-25 18:19:38 -0400

    add before hook

--- cypresse2e/cypress/integration/e2etests/menu.spec.js
+++ cypresse2e/cypress/integration/e2etests/menu.spec.js
@@ -2,4 +2,10 @@ import Menu from "../../pages/elements/menu";
 
 describe("Pokeworks Menu page", () => {
   const menu = new Menu();
+
+  context("User views menu page", () => {
+    beforeEach(() => {
+      menu.visit();
+    });
+  });
 });
