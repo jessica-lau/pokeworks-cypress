18de0d2a20bccc0d71927f7bb43bab2b9d35f20f
jessica-lau <jessica.lau2@gmail.com>
2023-04-26 21:44:43 -0400

    add element and getMethod for welcome heading

--- cypresse2e/cypress/pages/elements/menu.js
+++ cypresse2e/cypress/pages/elements/menu.js
@@ -1,8 +1,13 @@
 class Menu {
+  welcomeHeading = "h1";
   constructor() {}
 
   visit() {
     cy.visit("order.pokeworks.com");
+  }
+
+  getWelcomeHeading() {
+    return cy.get(this.welcomeHeading);
   }
 }
 
